126e0bd0743536bb7baedc6e8f3ef54f99969f5d
vreis <vinisrlima_@hotmail.com>
2024-06-11 23:24:21 -0300

    challenge finished

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
-<script setup>
-import { items } from "./movies.json";
-/*
- This is an Icon that you can use to represent the stars if you like
- otherwise you could just use a simple ⭐️ emoji, or * character.
-*/
-// import { StarIcon } from "@heroicons/vue/24/solid";
-</script>
-
 <template>
-  <!-- This is where your template goes	-->
-  <div></div>
+  <template v-for="movie in movies">
+    <v-card class="centralized-margin" max-width="400">
+      <v-img  
+        cover
+        max-height="600"
+        :src="movie.image"
+      />
+      <v-card-item>
+        <v-card-title class="my-2"  >{{ movie.name }}</v-card-title>
+        <template v-for="genre in movie.genres">
+          <v-chip class="me-2" color="primary" variant="flat">{{ genre }}</v-chip>
+        </template>
+      </v-card-item>
+      <v-card-text>
+       <div class="mb-10">{{ movie.description }}</div>
+       <div>Rating ({{ movie.rating }}/5): </div>
+      </v-card-text>
+    </v-card>
+  </template>
 </template>
+<script src="./index"></script> 
+<style lang="css" src="./style.css"></style>
